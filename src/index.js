f4181998e4bd5b97bece4057e35eca398cd38076
Nikita_dev <nikitasar64@gmail.com>
2023-04-21 20:22:47 +0300

    init: add swiper js, set slider in section projects. refactor: layout correction in section projects

--- src/index.js
+++ src/index.js
@@ -1,2 +1,51 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+
 import './styles/normalize.css';
 import './styles/style.scss';
+
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css';
+
+let i = 0;
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    modules: [Navigation, Pagination],
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    on: {
+        init: function () {
+          document.querySelectorAll('.projects__slide-name')[this.realIndex].classList.add('projects__slide-name--active');
+        },
+        slideChange: function (swiper) {
+          let activeIndex = swiper.realIndex
+          let slidesName = document.querySelectorAll('.projects__slide-name');
+
+          slidesName.forEach((item, index) => {
+            if (index === activeIndex) {
+              item.classList.add('projects__slide-name--active');
+            } else {
+              item.classList.remove('projects__slide-name--active')
+            }
+          });
+        }
+    },
+});
+
+let slidesName = document.querySelectorAll('.projects__slide-name');
+
+slidesName.forEach((slideName, index) => {
+  slideName.addEventListener('click', () => swiper.slideToLoop(index))
+})
+
+
+

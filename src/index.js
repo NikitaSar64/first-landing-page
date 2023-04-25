f289195bb41e96365b6d714ecbb07c1fcaea2a82
Nikita_dev <nikitasar64@gmail.com>
2023-04-25 20:53:39 +0300

    init: add section type repair, set play button for video test: testing page load speed with different format video.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css';
 
-let i = 0;
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
@@ -41,11 +40,28 @@ const swiper = new Swiper('.swiper', {
     },
 });
 
-let slidesName = document.querySelectorAll('.projects__slide-name');
+const slidesName = document.querySelectorAll('.projects__slide-name');
 
 slidesName.forEach((slideName, index) => {
   slideName.addEventListener('click', () => swiper.slideToLoop(index))
 })
 
+const video = document.querySelector('video');
+const videoWrapper = document.querySelector('.control__video-play');
+const videoPlayBtn = document.querySelector('.control__video-play-btn');
 
+window.addEventListener('DOMContentLoaded', () => {
+  video.insertAdjacentHTML('afterBegin', `
+    <source src='assets/video/test-video.webm' type='video/webm'>
+    <source src='assets/video/test-video.mp4' type='video/mp4'>
+  `)
+})
 
+videoPlayBtn.addEventListener('click', () => {
+  video.play();
+  videoWrapper.style.display = 'none';
+})
+
+video.addEventListener('ended', () => {
+  videoWrapper.style.display = 'flex';
+})

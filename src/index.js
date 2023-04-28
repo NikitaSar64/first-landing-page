68d31ea24be7d6bb1ae6bd3bcdf2135fd51f1d14
Nikita_dev <nikitasar64@gmail.com>
2023-04-28 21:46:28 +0300

    init: add section realization. Set slider in this section.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css';
 
-const swiper = new Swiper('.swiper', {
+// swiper
+
+const swiper = new Swiper('.swiper-projects', {
     direction: 'horizontal',
     loop: true,
     modules: [Navigation, Pagination],
@@ -22,29 +24,74 @@ const swiper = new Swiper('.swiper', {
     },
 
     on: {
-        init: function () {
-          document.querySelectorAll('.projects__slide-name')[this.realIndex].classList.add('projects__slide-name--active');
+        init: function() {
+          setStyleFirstActiveNameSlide('projects__slide-name', 'projects__slide-name--active');
         },
-        slideChange: function (swiper) {
-          let activeIndex = swiper.realIndex
-          let slidesName = document.querySelectorAll('.projects__slide-name');
-
-          slidesName.forEach((item, index) => {
-            if (index === activeIndex) {
-              item.classList.add('projects__slide-name--active');
-            } else {
-              item.classList.remove('projects__slide-name--active')
-            }
-          });
+        slideChange: function () {
+          setStyleActiveNameSlide(this, 'projects__slide-name', 'projects__slide-name--active')
         }
     },
 });
 
+const swiperLeft = new Swiper('.swiper-realization-left', {
+  direction: 'vertical',
+  loop: false,
+  spaceBetween: 55,
+  allowTouchMove: false,
+
+  on: {
+    init: function () {
+      setStyleFirstActiveNameSlide('realization__list-item', 'realization__list-item--active')
+    }, 
+    slideChange: function () {
+      setStyleActiveNameSlide(this, 'realization__list-item', 'realization__list-item--active')
+    }
+  }
+
+});
+
+const swiperRight = new Swiper('.swiper-realization-right', {
+  direction: 'vertical',
+  loop: false,
+  spaceBetween: 55,
+  allowTouchMove: false,
+  initialSlide: document.querySelectorAll('.realization__list-item').length - 1,
+});
+
+
+function setStyleFirstActiveNameSlide(sliderName, activeSliderName){
+  document.querySelector(`.${sliderName}`).classList.add(activeSliderName);
+}
+
+function setStyleActiveNameSlide(slider, sliderNames, activeSliderName){
+  let activeIndex = slider.realIndex;
+  let slidesName = document.querySelectorAll(`.${sliderNames}`);
+
+  slidesName.forEach((item, index) => {
+    if (index === activeIndex) {
+      item.classList.add(activeSliderName);
+    } else {
+      item.classList.remove(activeSliderName)
+    }
+  });
+}
+
 const slidesName = document.querySelectorAll('.projects__slide-name');
+const sliderBtn = document.querySelectorAll('.realization__list-item');
 
 slidesName.forEach((slideName, index) => {
   slideName.addEventListener('click', () => swiper.slideToLoop(index))
 })
+
+sliderBtn.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    swiperLeft.slideTo(index, 1000);
+    swiperRight.slideTo(sliderBtn.length - 1 - index, 1000);
+  })
+})
+
+
+// video
 
 const video = document.querySelector('video');
 const videoWrapper = document.querySelector('.control__video-play');
